cf4228d26c16f82595e5792a651ae32299a49e2f
norden-oscar <norden.oscar@gmail.com>
2015-01-29 05:22:42 +0100

    LEkt med JAXB

--- Homework 1/src/homework/pkg1/Homework1.java
+++ Homework 1/src/homework/pkg1/Homework1.java
@@ -5,6 +5,16 @@
  */
 package homework.pkg1;
 
+import com.sun.xml.internal.bind.api.JAXBRIContext;
+import com.w3schools.Degree;
+import com.w3schools.ObjectFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+
 /**
  *
  * @author norde_000
@@ -14,8 +24,24 @@ public class Homework1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         // TODO code application logic here
-    }
     
+        // Creates JAXContext instance
+        JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+       //Use the JAXBContext intsance to create unmarshaller
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        //unmarshall Degree.xml to get an instance of the JAXBElement
+        JAXBElement<Degree> unmarshalledObject = (JAXBElement<Degree>)unmarshaller.unmarshal(
+        ClassLoader.getSystemResourceAsStream("DegreeMall.xml"));
+        //get instance of required JAXB Root Class from JAXBElement
+        Degree degreeobj = unmarshalledObject.getValue();
+        System.out.print("Testing university" + degreeobj.getUniversity());
+    
+    
+    
+    
+    
+    }   
+        
 }
